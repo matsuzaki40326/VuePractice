a285c12415d6f87f7bd4ca982fac84af4caa801a
Matsuzaki Sota <98798507+matsuzaki40326@users.noreply.github.com>
2022-06-17 19:07:32 +0900

    77 ~ 79

---
+++ s6/test/src/modules/TestComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    test
+    {{ testData }}</div>
+</template>
+
+<script>
+export default {
+  name: 'TestComponent',
+  data() {
+    return {
+      testData: 'testData'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
