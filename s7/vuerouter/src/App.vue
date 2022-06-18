1704f3191663eea7a93aee7d3b0717565a2e1abd
Matsuzaki Sota <98798507+matsuzaki40326@users.noreply.github.com>
2022-06-18 10:53:31 +0900

    87,88

--- s7/vuerouter/src/App.vue
+++ s7/vuerouter/src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <nav>
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about" exact-active-class="test">About</router-link>
+      <!-- <router-link to="/about" tag="button">About</router-link> -->
     </nav>
     <router-view/>
   </div>
@@ -26,7 +27,15 @@ nav a {
   color: #2c3e50;
 }
 
+nav a.router-link-active {
+  color: red;
+}
+
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+nav a.test {
+  color: lightblue;
 }
 </style>
