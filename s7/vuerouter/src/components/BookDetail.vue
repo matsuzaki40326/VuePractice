0ae85af211f917dc9ecd3b29257e1c18a4d90389
Matsuzaki Sota <98798507+matsuzaki40326@users.noreply.github.com>
2022-06-18 11:37:35 +0900

    92

--- s7/vuerouter/src/components/BookDetail.vue
+++ s7/vuerouter/src/components/BookDetail.vue
@@ -3,6 +3,7 @@
     <p>本の詳細</p>
     <p>タイトル： {{ title }} </p>
     <p>本の内容： {{ content }} </p>
+    {{ books[bookIndex].title }}
   </div>
 </template>
 
@@ -12,6 +13,32 @@ export default {
     id: Number,
     title: String,
     content: String
+  },
+  data() {
+    return {
+      bookIndex: this.$route.params.id -1,
+      books: [
+        {
+          id: 1,
+          title: 'title1',
+          content: '本の内容1'
+        },
+        {
+          id: 2,
+          title: 'title2',
+          content: '本の内容2'
+        },{
+          id: 3,
+          title: 'title3',
+          content: '本の内容3'
+        },
+      ]
+    }
+  },
+  created() {
+    if (this.$route.params.id > this.books.length) {
+      this.$router.push('/book')
+    }
   }
 }
 </script>
