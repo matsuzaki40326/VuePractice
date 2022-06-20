56346baf391d88acf7ee8591440fb313e0176444
Matsuzaki Sota <98798507+matsuzaki40326@users.noreply.github.com>
2022-06-20 13:41:30 +0900

    108

--- s8/bookapp/src/App.vue
+++ s8/bookapp/src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <v-app>
     <Header />
-
     <v-main>
-      <router-view/>
+      <v-container>
+        <router-view @add-book-list="addBook"/>
+      </v-container>
     </v-main>
     <Footer />
   </v-app>
@@ -12,12 +13,48 @@
 <script>
 import Header from './global/Header.vue';
 import Footer from './global/Footer.vue';
+const STORAGE_KEY = 'books'
 
 export default {
     name: "App",
-    data: () => ({
-    //
-    }),
+    data() {
+      return {
+        books: [],
+        newBook: null
+      }
+    },
+    mounted() {
+      if (localStorage.getItem(STORAGE_KEY)) {
+        try {
+          this.books = JSON.parse(localStorage.getItem(STORAGE_KEY));
+        } catch(e) {
+          localStorage.removeItem(STORAGE_KEY);
+        }
+      }
+    },
+    methods: {
+      addBook(e) {
+
+        this.books.push({
+          id: this.books.length,
+          title: e.title,
+          image: e.image,
+          description: e.description,
+          readDate: '',
+          memo: ''
+        });
+        // this.newBook = '';
+        this.saveBooks();
+      },
+      removeBook(x) {
+        this.books.splice(x, 1);
+        this.saveBooks();
+      },
+      saveBooks() {
+        const parsed = JSON.stringify(this.books);
+        localStorage.setItem('STORAGE_KEY', parsed);
+      }
+    },
     components: {
       Header,
       Footer
