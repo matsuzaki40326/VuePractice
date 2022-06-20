2dd853921e60b5ef88b7f23954d849e64a947fc8
Matsuzaki Sota <98798507+matsuzaki40326@users.noreply.github.com>
2022-06-20 14:40:33 +0900

    110

--- s8/bookapp/src/pages/BookEdit.vue
+++ s8/bookapp/src/pages/BookEdit.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     BookEdit <br>
+    {{ book.title }} <br>
     {{ books }}
   </div>
 </template>
@@ -10,6 +11,19 @@ export default {
   name: 'BookEdit',
  props: {
   books: Array
+ },
+ data() {
+  return {
+    book: ''
+  }
+ },
+ beforeRouteEnter(to, from, next) {
+  next(vm => {
+    vm.$nextTick().then(() => {
+      vm.book = vm.books[vm.$route.params.id]
+      console.log(vm.book)
+    })
+  })
  }
 }
 </script>
