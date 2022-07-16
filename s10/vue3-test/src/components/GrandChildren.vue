11db2b1a871bf097bfb2e799bba0b8863cc13bb8
Matsuzaki Sota <98798507+matsuzaki40326@users.noreply.github.com>
2022-07-16 21:50:12 +0900

    134

---
+++ s10/vue3-test/src/components/GrandChildren.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    GrandChildren
+  </div>
+  <div>{{ userName }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return{}
+  },
+  inject: ['userName']
+}
+</script>
+
+<style>
+
+</style>
---
+++ s10/vue3-test/src/views/Children.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>Children</div>
+  <Grand-Children />
+</template>
+
+<script>
+import GrandChildren from '@/components/GrandChildren'
+export default {
+  components: {
+    GrandChildren
+  }
+
+}
+</script>
+
+<style>
+
+</style>
