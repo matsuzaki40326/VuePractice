a248fb55148bfefb64446b1eb23ccec10bb9fa25
Matsuzaki Sota <98798507+matsuzaki40326@users.noreply.github.com>
2022-07-17 20:38:45 +0900

    139

--- s10/vue3-test/src/views/CompositionTest.vue
+++ s10/vue3-test/src/views/CompositionTest.vue
@@ -3,19 +3,25 @@
     CompositionTest
     <p>{{ name }}</p>
     <p>{{ age }}</p>
+    <p>{{ nameRef }}</p>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
 export default {
   setup() {
     let name = 'test'
     const age = 30
+    const nameRef = ref('test20')
     console.log('setup')
     console.log(this)
+    console.log(nameRef)
+    console.log(nameRef.value)
     return {
       name,
-      age
+      age,
+      nameRef
     }
   },
   data() {
