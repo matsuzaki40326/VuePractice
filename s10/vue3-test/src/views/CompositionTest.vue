3647f52049a7622943f3940e0e68e73b4d0d735b
Matsuzaki Sota <98798507+matsuzaki40326@users.noreply.github.com>
2022-07-17 20:54:48 +0900

    141

--- s10/vue3-test/src/views/CompositionTest.vue
+++ s10/vue3-test/src/views/CompositionTest.vue
@@ -6,11 +6,13 @@
     <p>ref: {{ nameRef }}</p>
     <p>reactive: {{ book.title }}</p>
     <p>reactive: {{ book.author[0] }}</p>
+    <p>reactiveToRefs: {{ titleRef }}</p>
+    <p>reactiveToRefs: {{ authorRef[1] }}</p>
   </div>
 </template>
 
 <script>
-import { ref, reactive } from 'vue'
+import { ref, reactive, toRefs } from 'vue'
 export default {
   setup() {
     let name = 'test'
@@ -22,6 +24,11 @@ export default {
       author: ['a', 'y']
     })
 
+    const bookToRefs = reactive({
+      titleRef: 'titleRef',
+      authorRef: ['aRef', 'yRef']
+    })
+
     console.log('setup')
     console.log(this)
     console.log(nameRef)
@@ -30,7 +37,8 @@ export default {
       name,
       age,
       nameRef,
-      book
+      book,
+      ...toRefs(bookToRefs)
     }
   },
   data() {
